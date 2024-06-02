5a5ff10244580a52fc2c2a338230617ace1c909b
umarmrv <sh.murodovv@gmail.com>
2024-06-02 18:40:36 +0500

    Создано файл для authentication и написано модели для того чтобы посылать delete or post запросы

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from tastypie.resources import ModelResource
 from bookstore.models import Category, Books
+from tastypie.authorization import Authorization
+from .authentication import CustomAuthentication
 
 
 class CategoryResourse(ModelResource):
@@ -14,3 +16,5 @@ class BookResourse (ModelResource):
         queryset = Books.objects.all()
         resourse_name = 'books'
         allowed_methods = ['get', 'post', 'delete']
+        authentication = CustomAuthentication()
+        authorization = Authorization()
