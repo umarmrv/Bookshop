4f3f9c3a35f5a070fedeff5d7fbb8846fb803ac4
umarmrv <sh.murodovv@gmail.com>
2024-06-02 13:15:00 +0500

    Добавлено навигатционный панель basa.htmlи создал отдельный шаблон для каждой книги потом настроил views создал  функцию single_book который будет взаимодействовать с db

--- bookstore/views.py
+++ bookstore/views.py
@@ -8,3 +8,8 @@ def index(request):
 
     return render(request, "books.html", {'books': books})
    # return HttpResponse(''.join([str(book) + '<br>' for book in books]))
+
+
+def single_book(request, book_id):
+    books = models.Books.objects.get(pk=book_id)
+    return render(request, "single_book.html", {"books": books})
